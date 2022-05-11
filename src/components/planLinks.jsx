bba0c9dd0ea78055711f7dabfdfc775baac86551
Kaine Bruce <kaineyb@gmail.com>
2022-05-11 16:53:32 +0100

    changed planLinks to RouterLinks

--- src/components/planLinks.jsx
+++ src/components/planLinks.jsx
@@ -1,25 +1,31 @@
 import { CalendarIcon } from "@chakra-ui/icons";
-import { Box, Button, HStack } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 import { AiOutlineClockCircle, AiOutlineShoppingCart } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 function PlanLinks(props) {
   const { plan } = props;
   return (
     <Box borderWidth="1px" borderRadius="lg" p={5} mb={5}>
-      <HStack>
-        <Link to={`/plan/${plan.id}/shopping-list`}>
-          <Button leftIcon={<AiOutlineShoppingCart />}>Shopping List</Button>
+      <Flex direction={{ base: "column", sm: "rows" }} gap={2}>
+        <Link as={RouterLink} to={`/plan/${plan.id}/shopping-list`}>
+          <Button w="100%" leftIcon={<AiOutlineShoppingCart />}>
+            Shopping List
+          </Button>
         </Link>
 
-        <Link to={`/plan/${plan.id}/schedule`}>
-          <Button leftIcon={<AiOutlineClockCircle />}>Schedule</Button>
+        <Link as={RouterLink} to={`/plan/${plan.id}/schedule`}>
+          <Button w="100%" leftIcon={<AiOutlineClockCircle />}>
+            Schedule
+          </Button>
         </Link>
 
-        <Link to={`/plan/${plan.id}/recipes`}>
-          <Button leftIcon={<CalendarIcon />}>Recipes</Button>
+        <Link as={RouterLink} to={`/plan/${plan.id}/recipes`}>
+          <Button w="100%" leftIcon={<CalendarIcon />}>
+            Recipes
+          </Button>
         </Link>
-      </HStack>
+      </Flex>
     </Box>
   );
 }
